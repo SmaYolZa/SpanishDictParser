2c7c87c71ac286c7d29504e18cf6d47b5ecbc849
SmaYolZa <SmaYolZa@protonmail.ch>
2018-05-19 10:15:04 -0400

    This commit updates the code, cleaning it up, and optimizing it.

--- SpanishDictParser.py
+++ SpanishDictParser.py
@@ -3,13 +3,106 @@ import bs4
 import collections
 
 
-def getting_url(word):
+def checking_word_for_conjugation(word):
+    """
+    This method gets and searches for definition and conjugation of the given word on SpanishDict
+    :param word: word that user want to be conjugated
+    :return: returns the definition and conjugation  of the given word
+    """
+
+    # TODO: add the ability to print out basic conjugations for the word
+
     url = 'http://www.spanishdict.com/conjugate/{0}'.format(word)
-    response = requests.get(url=url)
-    return response.text
+    url_return = requests.get(url=url)
+    url_return = url_return.text
+
+    soup = bs4.BeautifulSoup(url_return, "html.parser")
+
+    """
+    This part does Error Handling.
+    In case there is no conjugation for the word, 
+    or the word fits in a different category 
+    this section will try to figure out what to do to get the best result
+    """
+
+    # TODO: add redirects to other types in case this one result's in error
+
+    try:
+        word_definition = soup.find(class_='el').get_text()
+    except AttributeError:
+        return 'ERROR'
+    else:
+        return text_clean_up(word_definition)
 
 
-def getting_basic_info(html):
-    soup = bs4.BeautifulSoup(html, "html.parser")
-    definition = soup.find(class_='el').get_text()
-    return definition
+def checking_phrase_for_translation(phrase):
+    """
+    This method checks for translation of the given phrase and returns it to the user
+    :param phrase: Phrase that needs to be translated
+    :return: Returns the translation of the given phrase of an error
+    """
+
+    url = 'http://www.spanishdict.com/translation/{0}'.format(phrase)
+    url_return = requests.get(url=url)
+    url_return = url_return.text
+
+    soup = bs4.BeautifulSoup(url_return, "html.parser")
+
+    """
+    This part does Error Handling.
+    In case the phrase or word fits in a different category or web page redirects it,
+    this part will handle these possible errors 
+    """
+
+    # TODO: add redirects to other types in case this one result's in error
+
+    try:
+        phrase_translation = soup.find(class_='translate').get_text()
+    except AttributeError:
+        return 'ERROR'
+    else:
+        return text_clean_up(phrase_translation)
+
+
+def looking_up(phrase):
+    """
+    This method looks up the word or phrase
+    * THIS ONLY TRANSLATES SPANISH TO ENGLISH *
+    :param phrase: Word of phrase for look up
+    :return: Returns the definition or translation of the given phrase of word
+    """
+
+    url = 'http://www.spanishdict.com/translation/{0}?t=1&langFrom=en'.format(phrase)
+    url_return = requests.get(url=url)
+    url_return = url_return.text
+
+    look_up_results = collections.namedtuple('look_up_results', 'microsoft, sdl, promt')
+
+    soup = bs4.BeautifulSoup(url_return, "html.parser")
+
+    result_microsoft = soup.find(id='mt-to-es').find(class_='mt-info-text').get_text()
+    result_sdl = soup.find(class_='mt-info sdl mt-from-en').find(class_='mt-info-text').get_text
+    result_promt = soup.find(class_='mt-info promt mt-from-en').find(class_='mt-info-text').get_text
+
+    result_microsoft = text_clean_up(result_microsoft)
+    result_sdl = text_clean_up(result_sdl)
+    result_promt = text_clean_up(result_promt)
+
+    return look_up_results(microsoft=result_microsoft, sdl=result_sdl, promt=result_promt)
+
+
+def text_clean_up(text: str):
+    """
+    This method cleans up the text which was scraped from web page by removing whitespaces
+    :param text: The input text
+    :return: Cleaned-up output text
+    """
+
+    if not text:
+        return text
+
+    text = text.strip()
+    return text
+
+
+
--- program.py
+++ program.py
@@ -5,10 +5,18 @@ from time import sleep
 
 def main():
     print_logo()
-    word = input("What do you want to translate : ")
-    url = SpanishDictParser.getting_url(word)
-    print(SpanishDictParser.getting_basic_info(url))
-    what_to_do_next()
+    to_do = what_to_do()
+
+    phrase = input("\nWhat do you want to translate : ")
+
+    if to_do == 1:
+        case_of_conjugation(word=phrase)
+    elif to_do == 2:
+        case_of_translation(phrase=phrase)
+    elif to_do == 3:
+        case_of_lookup(phrase=phrase)
+
+    stop_or_not()
 
 
 def print_logo():
@@ -17,17 +25,55 @@ def print_logo():
     print('*******************************************')
 
 
-def what_to_do_next():
-    print('Do you want to look something else up?')
-    choice = input('Choice [Y] or [N] : ')
-    choice.islower()
-    if choice == 'y':
-        system('cls')
-        main()
-    elif choice == 'n':
-        print('Goodbye!')
-        sleep(1)
-        exit()
+def stop_or_not():
+    choice = None
+    while choice != 'n':
+        print('\nDo you want to look something else up?')
+        choice = input('Choice [Y] or [N] : ')
+        choice = choice.lower().strip()
+
+        if choice == 'y':
+            system('cls')
+            main()
+        elif choice == 'n':
+            system('cls')
+            print('Goodbye!')
+            sleep(1)
+            exit()
+        elif choice != 'y' and choice != 'n':
+            print('Sorry, I did not understand "{0}", could you try again...'.format(choice))
+
+
+def what_to_do():
+    choice = None
+    while choice != '1' or choice != '2' or choice != '3':
+        print('\nWhat do you want me to do?')
+        print('[1] Conjugate'
+              '\n[2] Translate'
+              '\n[3] Look up')
+        choice = input('Choice : ')
+        choice = choice.lower().strip()
+
+        if choice == '1':
+            return 1
+        elif choice == '2':
+            return 2
+        elif choice == '3':
+            return 3
+        else:
+            print('Sorry I did not understand "{0}", please try again'.format(choice))
+
+
+def case_of_conjugation(word):
+    print(SpanishDictParser.checking_word_for_conjugation(word=word))
+
+
+def case_of_translation(phrase):
+    print(SpanishDictParser.checking_phrase_for_translation(phrase=phrase))
+
+
+def case_of_lookup(phrase):
+    print(SpanishDictParser.looking_up(phrase=phrase))
 
 
 if __name__ == '__main__':
